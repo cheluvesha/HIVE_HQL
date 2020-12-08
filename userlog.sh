6abfcb34655b4d52bde77d754565e29d2b7d50e6
cheluvesha <cheluvesh.b@gmail.com>
2020-12-08 15:05:44 +0530

    [CHELUVESHA]Added:Hql script for Finding average hours

---
+++ userlog.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+echo "Script is running"
+
+hive -f /home/cheluvesha/Documents/hive/userlogView1.hql  
+
+hive -e "select * from fellowship.userlog_avg_hour;" > userlog.txt
+
+echo "lowest average hours"
+
+sort -n -k2 userlog.txt
+
+echo "highest average hours"
+
+sort -r -k2 userlog.txt
